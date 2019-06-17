7765e70d823ff2c79a4695d5025f03ae28f7e326
Arpit Kapoor <i-arpitkapoor>
2019-06-17 18:36:12 +0530

    bug fixes

--- src/app.js
+++ src/app.js
@@ -129,7 +129,7 @@ app.post('/register', function(req, res){
     User.register(new User({username: req.body.username, email: req.body.email}), req.body.password, function(err, user){
         if(err){
             console.log(err)
-            return res.render('register');
+            return res.redirect('/log');
         }
         passport.authenticate("local")(req, res, function(){
             res.redirect('/')
